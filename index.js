9b5481fc303d3bef3bb10701f136c818c29ae52e
Nevio <nevio.vesic@gmail.com>
2012-06-17 12:42:57 +0200

    Adding some changes. Just to be here. Nothing else

---
+++ index.js
@@ -0,0 +1,8 @@
+
+/**
+ * telapi
+ * Copyright (c) 2012 TelTech Systems Inc. <info@telapi.com>
+ * MIT Licensed
+ */
+
+ module.exports = require('./lib/telapi')
---
+++ tests/inboundxml.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+
+var util       = require("util")
+var inboundxml = require("../lib/inboundxml.js")
+
+util.log("Starting out TelAPI InboundXML tests ...")
+
