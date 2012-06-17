263dc77074623aea02c8ae3067a1c1602dded9af
Nevio <nevio.vesic@gmail.com>
2012-06-17 14:26:13 +0200

    Adding partial InboundXML logic with attribute and element validations

--- tests/inboundxml.js
+++ tests/inboundxml.js
@@ -1,7 +1,22 @@
 #! /usr/bin/env node
 
 var util       = require("util")
-var inboundxml = require("../lib/inboundxml.js")
+var InboundXML = require("../lib/inboundxml")
+var testsch    = require("../lib/schemas/telapi.json")
 
 util.log("Starting out TelAPI InboundXML tests ...")
 
+util.log("...........................................")
+
+// Starting out SAY Element
+util.log("SAY ELEMENT START")
+util.log( new InboundXML.Say("I am saying InboundXML from Node.JS helper", { voices : "man" }).toString())
+util.log("SAY ELEMENT END")
+
+util.log("...........................................")
+
+
+
+util.log("...........................................")
+
+//util.log(TelapiSchema.inboundxml.verbs.Say.attributes)
