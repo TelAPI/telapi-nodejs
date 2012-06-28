6e5f3f0b3546021d939f48d358618b651628032a
Nevio <nevio.vesic@gmail.com>
2012-06-28 16:34:17 +0200

    #3 - Adding play element with mix of Say element

---
+++ examples/inboundxml/play.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+/**
+ * 
+ * Say something with helo of InboundXML and TelAPI
+ *
+ * --------------------------------------------------------------------------------
+ * 
+ * @category  TelApi Helper
+ * @package   TelApi
+ * @author    Nevio Vesic <nevio@telapi.com>
+ * @license   http://creativecommons.org/licenses/MIT/ MIT
+ * @copyright (2012) TelTech Systems, Inc. <info@telapi.com>
+ */
+
+var util        = require("util"); // For loging purposes :)
+
+// Get inbound he
+var InboundXML  = require('telapi').inboundxml;
+
+// Initialize TelAPI response element. This is A MUST!
+var response = new InboundXML.Response();
+
+// Append Say element into response
+response.append(
+	new InboundXML.Say(
+		'Example usage of TelAPI Node.JS InboundXML Play element', 
+		{ voice : 'man' }
+	)
+);
+
+// Append Play element into response
+response.append(
+	new InboundXML.Play(
+		"http://funny-stuff.audio4fun.com/download/sounds/87.mp3"
+	)
+);
+
+util.log("\n\nInboundXML Say and Play Element: \n\n" + response.toString())
