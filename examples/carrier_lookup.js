68fc4b116b9ae23eddfa79ba1a82927a93c6bc82
Nevio <nevio.vesic@gmail.com>
2012-11-09 13:35:58 +0100

    Adding carrier and cnam examples

---
+++ examples/carrier_lookup.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+/**
+ * 
+ * 
+ * Only US phone numbers are available.
+ * 
+ * --------------------------------------------------------------------------------
+ * 
+ * @category  TelApi Helper
+ * @package   TelApi
+ * @author    Nevio Vesic <nevio@telapi.com>
+ * @license   http://creativecommons.org/licenses/MIT/ MIT
+ * @copyright (2012) TelTech Systems, Inc. <info@telapi.com>
+ */
+
+var util       = require("util"); // For logging purposes :)
+
+var Client     = require('telapi').client;
+
+// Load configuration file
+var configuration = require('./configuration');
+
+// Setup TelAPI Client
+var client = new Client(configuration.account_sid, configuration.auth_token);
+ 
+client.create(
+    'carrier',
+    { 'PhoneNumber' : '{PhoneNumber_E.164}' },
+    function(response) { // SUCCESS CALLBACK
+        for(var carrier in response.carrier_lookups) {
+            util.log(
+                "Carrier lookup cost " + 
+                response.carrier_lookups[carrier].price + 
+                ". Phone Number carrier is: " + 
+                response.carrier_lookups[carrier].carrier + "."
+            );
+        }
+    },
+    function(error) { // ERROR CALLBACK
+        util.log("Usage Error: " + error)
+    }
+);
+
+
---
+++ examples/cnam_lookup.js
@@ -0,0 +1,45 @@
+#! /usr/bin/env node
+
+/**
+ * 
+ * 
+ * Only US phone numbers are available.
+ * 
+ * --------------------------------------------------------------------------------
+ * 
+ * @category  TelApi Helper
+ * @package   TelApi
+ * @author    Nevio Vesic <nevio@telapi.com>
+ * @license   http://creativecommons.org/licenses/MIT/ MIT
+ * @copyright (2012) TelTech Systems, Inc. <info@telapi.com>
+ */
+
+var util       = require("util"); // For logging purposes :)
+
+var Client     = require('telapi').client;
+
+// Load configuration file
+var configuration = require('./configuration');
+
+// Setup TelAPI Client
+var client = new Client(configuration.account_sid, configuration.auth_token);
+ 
+client.create(
+    'cnam',
+    { 'PhoneNumber' : '{PhoneNumber_E.164}' },
+    function(response) { // SUCCESS CALLBACK
+        for(var cnam in response.cnam_dips) {
+            util.log(
+                "CNAM lookup cost " + 
+                response.cnam_dips[cnam].price + 
+                ". Cnam body is: " + 
+                response.cnam_dips[cnam].body + "."
+            );
+        }
+    },
+    function(error) { // ERROR CALLBACK
+        util.log("Usage Error: " + error)
+    }
+);
+
+
